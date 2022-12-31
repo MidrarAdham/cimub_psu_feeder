22c9aeb426f8829f536afa135c7aadd4778eb6fd
MidrarAdham <midrar@pdx.edu>
2022-12-31 12:01:20 -0800

    rearranged house profiles

---
+++ testing_opendss/run_dss.py
@@ -0,0 +1,22 @@
+import sys
+
+# sys.path.append('/home/deras/.local/lib/python3.8/site-packages/opendss_wrapper')
+
+import OpenDSS_wrapper
+'''
+dssengine = dss.run_dss()
+
+dssengine.Command = 'Compile ./Master.dss'
+
+node_names = dssEngine.Circuit.AllNodeNames
+
+
+node_names = dssengine.Circuit.NodePower
+
+results = {}
+
+for i in range(len(node_names)):
+    results[node_names[i]] = {'power': node_powers[i]}
+
+print(results)
+'''
