bedfa070a16467e9aacf58a7f201be1f5eee1f67
MidrarAdham <midrar@pdx.edu>
2023-01-04 13:06:56 -0800

    init gitignore

--- testing_opendss/run_dss.py
+++ testing_opendss/run_dss.py
@@ -1,22 +1,2 @@
-import sys
+from OpenDSS import DSS as dss
 
-# sys.path.append('/home/deras/.local/lib/python3.8/site-packages/opendss_wrapper')
-
-import OpenDSS_wrapper
-'''
-dssengine = dss.run_dss()
-
-dssengine.Command = 'Compile ./Master.dss'
-
-node_names = dssEngine.Circuit.AllNodeNames
-
-
-node_names = dssengine.Circuit.NodePower
-
-results = {}
-
-for i in range(len(node_names)):
-    results[node_names[i]] = {'power': node_powers[i]}
-
-print(results)
-'''
